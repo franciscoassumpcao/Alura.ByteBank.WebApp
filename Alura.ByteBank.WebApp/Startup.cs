e52832d4b398e008467bcfdab58e0d21d3ba9754
franciscoassumpcao <38674113+franciscoassumpcao@users.noreply.github.com>
2023-07-10 15:27:43 +0200

    New commit

--- Alura.ByteBank.WebApp/Startup.cs
+++ Alura.ByteBank.WebApp/Startup.cs
@@ -28,7 +28,7 @@ namespace Alura.ByteBank.WebApp
             Configuration = configuration;
         }
         public IConfiguration Configuration { get; }
-        // Adicionando comentario.
+        // Adicionando comentario. 2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
